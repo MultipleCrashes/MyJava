24d5cdc60dae714ca00fc5aab1fca83aae65a098
root <root@ubuntu-HP-G60-Notebook-PC.(none)>
2014-04-03 02:07:46 +0530

    Added function to display links in the linked list

--- LinkListApp.java
+++ LinkListApp.java
@@ -1,5 +1,4 @@
 import java.io.*;
-
 class link
 {
 link next;
@@ -9,10 +8,10 @@ link(int data)
  val=data;
 }
 
-//public void displayLink()
-//{
-//System.out.println("This node has value "+data);
-//}
+public void displayLink()
+{
+System.out.println(val);
+}
 
 }
 
@@ -23,14 +22,28 @@ LinkList()
 {
 first=null;
  }
+
+
 public void append(int data)
 {
 link newlink=new link(data);
 newlink.next=first;
 first=newlink;
-//newlink.displayLink();
 System.out.println("Successfully Inserted Data "+data);
 }
+
+public void displayLinkList()
+{
+System.out.println("\n\nDisplaying all the inserted data\n\n");
+link current=first;
+while(current!=null)
+{
+ current.displayLink();
+ current=current.next;
+}
+
+}
+
 }
 
 
@@ -41,6 +54,13 @@ public static void main(String args[]) throws IOException
 System.out.println("Inside Linked List App");
 LinkList obj=new LinkList();
 obj.append(10);
+obj.append(20);
+obj.append(5);
+obj.append(4);
+for(int i=0;i<=10;i++)
+   obj.append(i);
+
+obj.displayLinkList();
 
 }
 }
