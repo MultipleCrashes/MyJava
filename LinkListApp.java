67bcb124ae7225f34f4ce47036cec886948a97ac
root <root@ubuntu-HP-G60-Notebook-PC.(none)>
2014-04-03 02:50:29 +0530

    Modified alignment

--- LinkListApp.java
+++ LinkListApp.java
@@ -1,68 +1,64 @@
 import java.io.*;
 class link
 {
-link next;
-int val;
-link(int data)
-{
+ link next;
+ int val;
+ link(int data)
+ {
  val=data;
-}
+ }
 
-public void displayLink()
-{
-System.out.println(val);
-}
-
+ public void displayLink()
+ {
+ System.out.println(val);
+ }
 }
 
 class LinkList
 {
-link first;
-LinkList()
-{
-first=null;
+ link first;
+ LinkList()
+ {
+ first=null;
  }
 
+ public void append(int data)
+ {
+ link newlink=new link(data);
+ newlink.next=first;
+ first=newlink;
+ System.out.println("Successfully Inserted Data "+data);
+ }
 
-public void append(int data)
-{
-link newlink=new link(data);
-newlink.next=first;
-first=newlink;
-System.out.println("Successfully Inserted Data "+data);
-}
-
-public void displayLinkList()
-{
-System.out.println("\n\nDisplaying all the inserted data\n\n");
-link current=first;
-while(current!=null)
-{
- current.displayLink();
- current=current.next;
-}
-
-}
-
+ public void displayLinkList()
+ {
+ System.out.println("\n\nDisplaying all the inserted data\n\n");
+ link current=first;
+  while(current!=null)
+  {
+  current.displayLink();
+  current=current.next;
+  }
+ }
 }
 
 
 public class LinkListApp
 {
-public static void main(String args[]) throws IOException
-{
-System.out.println("Inside Linked List App");
-LinkList obj=new LinkList();
-obj.append(10);
-obj.append(20);
-obj.append(5);
-obj.append(4);
-for(int i=0;i<=10;i++)
+ public static void main(String args[]) throws IOException
+ {
+ System.out.println("Inside Linked List App");
+ LinkList obj=new LinkList();
+ obj.append(10);
+ obj.append(20);
+ obj.append(5);
+ obj.append(4);
+ for(int i=0;i<=10;i++)
    obj.append(i);
 
-obj.displayLinkList();
+ obj.displayLinkList();
 
-}
+ }
 }
 
 
